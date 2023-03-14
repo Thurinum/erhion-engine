a2fce69a36c99a7f4b28b08a82456989c7949148
Thurinum <m.gagnon941@gmail.com>
2023-03-14 17:54:03 -0400

    feat(engine): Add GLAD dependency and show window
    Add GLAD as dependency. Allows displaying a window with BeginRenderLoop().

--- app/main.cpp
+++ app/main.cpp
@@ -7,5 +7,10 @@ using namespace Erhion;
 int main() {
     Engine::Window w("Erhion Engine");
 
+    w.BeginRenderLoop([]() {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    });
+
     return EXIT_SUCCESS;
 }
